5a35f5ac71f1332a908557942e3da3f63554a2f7
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2021-10-04 19:17:23 -0400

    Mon Oct  4 07:17:23 PM EDT 2021

--- config.h
+++ config.h
@@ -185,18 +185,19 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_Return,	togglescratch,	{.ui = 0} },
 
 	{ MODKEY,			XK_z,		incrgaps,	{.i = +3 } },
-	/* { MODKEY|ShiftMask,		XK_z,		spawn,		SHCMD("") }, */
+	{ MODKEY|ShiftMask,		XK_z,		spawn,		SHCMD("ume -x 'ytfzf -a'") },
 	{ MODKEY,			XK_x,		incrgaps,	{.i = -3 } },
 	/* { MODKEY|ShiftMask,		XK_x,		spawn,		SHCMD("") }, */
-	/* { MODKEY,			XK_c,		spawn,		SHCMD("emacsclient -nc -a='' --eval '(org-capture)'") }, */
-	{ MODKEY,			XK_c,		spawn,		SHCMD(TERMINAL " -e ipython") },
+	{ MODKEY,			XK_c,		spawn,		SHCMD("emacsclient -nc -a='' --eval '(org-capture)'") },
+	/* { MODKEY,			XK_c,		spawn,		SHCMD(TERMINAL " -e ipython") }, */
 	/* { MODKEY|ShiftMask,		XK_c,		spawn,		SHCMD("") }, */
 	/* V is automatically bound above in STACKKEYS */
 	{ MODKEY,			XK_b,		togglebar,	{0} },
 	/* { MODKEY|ShiftMask,		XK_b,		spawn,		SHCMD("") }, */
 	{ MODKEY,			XK_n,		spawn,		SHCMD(TERMINAL " -e nvim -c VimwikiIndex") },
 	{ MODKEY|ShiftMask,		XK_n,		spawn,		SHCMD(TERMINAL " -e newsboat; pkill -RTMIN+6 dwmblocks") },
-	{ MODKEY,			XK_m,		spawn,		SHCMD(TERMINAL " -e ncmpcpp") },
+	/* { MODKEY,			XK_m,		spawn,		SHCMD(TERMINAL " -e ncmpcpp") }, */
+	{ MODKEY,			XK_m,		spawn,		SHCMD("mysdcv.sh") },
 	{ MODKEY|ShiftMask,		XK_m,		spawn,		SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
 	{ MODKEY,			XK_comma,	spawn,		SHCMD("mpc prev") },
 	{ MODKEY|ShiftMask,		XK_comma,	spawn,		SHCMD("mpc seek 0%") },
